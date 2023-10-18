317e522b9d3b73a14bbbf50a4c44487c14e3ce17
Antonio Fonseca <lamgers@gmail.com>
2023-10-17 19:48:37 -0600

    se agrega url mongo a env

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 async function dbConnect(): Promise<void> {
   const DB_URI =
-    <string>'mongodb+srv://angel:123@cluster0.rlqxziz.mongodb.net/?retryWrites=true&w=majority';
+    <string>process.env.URL_MONGO;
   await connect(DB_URI);
 }
 
